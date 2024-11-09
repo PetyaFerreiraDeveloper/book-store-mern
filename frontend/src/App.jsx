0bdaefcc0cbb6466100028e7f1419e841331dea0
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-09 13:25:38 +0100

    define fonts and colors

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,11 +2,13 @@ import { Outlet } from "react-router-dom"
 
 function App() {
   return (
-    <>
+    <div className="max-w-screen-2xl mx-auto">
       <nav>Navbar</nav>
-      <Outlet />
+      <main className="min-h-screen bg-red-200 px-4 py-6 font-primary">
+        <Outlet />
+      </main>
       <footer>Footer</footer>
-    </>
+    </div>
   )
 }
 
