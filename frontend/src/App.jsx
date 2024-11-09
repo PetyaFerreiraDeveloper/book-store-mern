a29394e6e4f9d85e62a98ad298cc69b8ced32240
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-09 12:04:50 +0100

    start the project

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { Outlet } from "react-router-dom"
+
+function App() {
+  return (
+    <>
+      <nav>Navbar</nav>
+      <Outlet />
+      <footer>Footer</footer>
+    </>
+  )
+}
+
+export default App
