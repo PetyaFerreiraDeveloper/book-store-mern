65616e8ca9a54a6c34f900abbbe16da7fed4f1ff
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-12 23:16:01 +0100

    create navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
+
 import { BsCart3 } from "react-icons/bs"
 import { HiOutlineMenuAlt1 } from "react-icons/hi"
 import { IoSearchSharp } from "react-icons/io5"
 import { IoPersonOutline } from "react-icons/io5"
 import { IoHeartOutline } from "react-icons/io5"
-import { Link } from "react-router-dom"
+
+import avatarImg from "../assets/avatar.png"
+
+const navigation = [
+  { name: "Dahsboard", href: "/dashboard" },
+  { name: "Orders", href: "/orders" },
+  { name: "Cart Page", href: "/cart" },
+  { name: "Checkout", href: "/checkout" },
+]
 
 const Navbar = () => {
-  const currentUser = false
+  const [isDropdpwnOpen, setIsDropdownOpen] = useState(false)
+  console.log(isDropdpwnOpen)
+
+  const currentUser = true
   return (
     <header className="flex justify-between xl:justify-start xl:gap-24 items-center px-6 xl:px-24 pt-9 pb-5 w-full">
       <Link className="p-2" to="/">
@@ -24,22 +38,38 @@ const Navbar = () => {
         />
       </div>
 
-      <nav className="flex xl:ml-auto gap-8">
-        <button className="hidden xl:block">
-          <div>
-            {currentUser ? (
-              <>
-              <button>
-                <img src="" alt="avatar" />
+      <nav className="flex items-center xl:ml-auto gap-8">
+        <div className="hidden xl:block relative">
+          {currentUser ? (
+            <>
+              <button
+                className="flex justify-center items-center"
+                onClick={() => setIsDropdownOpen(!isDropdpwnOpen)}
+              >
+                <img
+                  src={avatarImg}
+                  alt="avatar"
+                  className="w-7 h-7 ring-2 rounded-full bg-blue-500"
+                />
               </button>
-              </>
-            ) : (
-              <Link to="/login">
-                <IoPersonOutline className="w-6 h-6" />
-              </Link>
-            )}
-          </div>
-        </button>
+              {isDropdpwnOpen && (
+                <div className="absolute right-0 bg-white shadow-lg rounded-md w-48 mt-2 py-4 z-40">
+                  <ul className="py-2">
+                    {navigation.map((nav) => (
+                      <li key={nav.name} onClick={() => setIsDropdownOpen(false)}>
+                        <Link to={nav.href} className=" block py-2 px-4 hover:bg-slate-300">{nav.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </div>
+              )}
+            </>
+          ) : (
+            <Link to="/login">
+              <IoPersonOutline className="w-6 h-6" />
+            </Link>
+          )}
+        </div>
         <button className="hidden xl:block">
           <IoHeartOutline className="w-6 h-6" />
         </button>
