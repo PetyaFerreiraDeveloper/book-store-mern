c3a2af746ce3cf5398010b12e2fdd2145288ecf2
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-17 10:28:47 +0100

    create method for getting the img url from the news

--- frontend/src/utils/getImgUrl.js
+++ frontend/src/utils/getImgUrl.js
@@ -2,4 +2,8 @@ function getImgUrl(img) {
   return new URL(`../assets/books/${img}`, import.meta.url)
 }
 
-export { getImgUrl }
+function getNewsImgUrl(img) {
+  return new URL(`../assets/news/${img}`, import.meta.url)
+}
+
+export { getImgUrl, getNewsImgUrl }
