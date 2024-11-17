616a62b8916bed09ada63dc7e68af64585cc9778
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-17 15:27:53 +0100

    create Error page for when a route is missing

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -1,17 +1,20 @@
 import { createBrowserRouter } from "react-router-dom"
 import App from "../App"
 import Home from "../pages/home/Home"
+import Login from "../pages/Login"
+import Error from "../pages/Error"
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
+    errorElement: <Error />,
     children: [
       {
         path: "/",
         element: <Home />,
       },
-      { path: "/about", element: <h1>About</h1> },
+      { path: "/login", element: <Login /> },
       { path: "/orders", element: <h1>Orders</h1> },
     ],
   },
