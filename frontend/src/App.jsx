d18cf05d2af98bb237f2bc64162cb1a026e3666b
Petya Naydenova Ferreira <petianaidenova@gmail.com>
2024-11-23 10:15:34 +0100

    update style

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,9 +5,9 @@ import Footer from "./components/Footer"
 
 function App() {
   return (
-    <div className="max-w-screen-2xl mx-auto">
+    <div className="flex flex-col h-screen max-w-screen-2xl mx-auto">
       <Navbar />
-      <main className="min-h-screen bg-red-200 px-4 py-6 font-primary">
+      <main className="flex-1 px-4 py-6 font-primary">
         <Outlet />
       </main>
       <Footer />
